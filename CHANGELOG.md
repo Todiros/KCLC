b9233a0665c17bd6eda05a2e948c4805824ded5a
Xzahn <todiros@gmail.com>
2016-06-17 22:06:38 +0300

    Added blog page with header only, unaccesible by the public yet

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,13 +1,29 @@
 # VERSION HISTORY
 All notable changes to this project will be documented in this file.
 
+### LEGEND
+#### *Added* - file/features added to the current release
+#### *Updated* - existing working features updated
+#### *Fixed* - fixed broken or incorrectly working features
+#### *Removed* - file/features removed from the current release
+#### *In Development* - file/feature added to the current release for testing and/or further development (usually unaccesibly by the public) 
+#### *Released* - complete file/feature in the current release is now available to the public and no longer *In Development*
+
+## [0.1.1] - 2016/06/17
+### Added
+- Blog page *In Development* 
+..* blog header
+
+### Updated
+- Footer and Header now use Bootstrap 3.3.0 and jQuery 1.11.1 from the internet instead of localy
+
 ## [0.0.1] - 2016/06/12
 ### Added
 - Added all the files
 
-### Update
-- Update .gitignore file
+### Updated
+- Updated .gitignore file
 
 ## [0.0.0] - 2016/06/12
 ### Added
-* Created repository and added log file
+- Created repository and added log file
---
+++ blog.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<title>BLOG.YNOTLEARN</title>
+		<!-- STYLE INCS -->
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/bootstrap/3.3.0/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/bootstrap/3.3.0/css/bootstrap-theme.min.css">
+		<link rel="stylesheet" href="styles/css/src/blog.css">
+	</head>
+
+	<body>
+
+		<div class="container-fluid">
+			<div class="row">
+				<header id="headerStyling">
+						<ul>
+
+							<li class="col-xs-3 col-sm-4 text-right">
+								<div class="dropdown">
+									<a class="dropdown-toggle" type="button" id="dropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">menu <span class="caret"></span></a>
+
+									<ul class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu1">
+										<!-- TODO: Button group! -->
+										<li><a href="#">Login</a> / <a href="#">Logout</a></li>
+										<li role="separator" class="divider"></li>
+										<li><a href="#">Action</a></li>
+										<li><a href="#">Do SMTH</a></li>
+										<li><a href="#">Do SMTH else</a></li>
+									</ul>
+								</div>
+							</li>
+							<li class="col-xs-6 col-sm-4">
+								<a id="logo" href="#">
+									<div id="logoImg"></div>
+								</a>
+							</li>
+							<li class="col-xs-3 col-sm-4">
+								<a id="site" href="#">SITE</a>
+							</li>
+							
+						</ul>
+				</header> 
+			</div>
+		</div>	
+
+
+
+
+		</div>
+
+		<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+	</body>
+
+</html>
