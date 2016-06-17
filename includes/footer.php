dc97fa7628ff6e8b0f0c495e279bf03b1df62e4d
Xzahn <todiros@gmail.com>
2016-06-17 22:04:10 +0300

    Bootstrap and jQuery are now loaded from the internet in footer.php and header.php

--- includes/footer.php
+++ includes/footer.php
@@ -5,10 +5,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		
 		<!-- STYLE INCS -->
-		<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">-->
-		<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap-theme.min.css">-->
-		<link rel="stylesheet" href="res/lib/bootstrap.min.css">
-		<link rel="stylesheet" href="res/lib/bootstrap-theme.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap-theme.min.css">
 		<link rel="stylesheet" type="text/css" href="../styles/css/src/footer.css">
 	</head>
 	
@@ -32,9 +30,8 @@
 					</div>
 			</footer>
 		</div>
-	<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>-->
-	<script src="res/lib/jquery-1.11.1.min.js"></script>
-	<script src="res/lib/bootstrap.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 	</body>
 	
 </html>
--- includes/header.php
+++ includes/header.php
@@ -3,12 +3,8 @@
 	<head>
 		
 		<!-- STYLE INCS -->
-		<!--
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap-theme.min.css">
-		-->
-		<link rel="stylesheet" href="res/lib/bootstrap.min.css">
-		<link rel="stylesheet" href="res/lib/bootstrap-theme.min.css">
 		<link rel="stylesheet" type="text/css" href="../styles/css/src/header.css">
 	</head>
 
@@ -40,9 +36,8 @@
 					</div>
 			</header>
 		</div>
-		<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>-->
-		<script src="res/lib/jquery-1.11.1.min.js"></script>
-		<script src="res/lib/bootstrap.min.js"></script>
-	<script type="text/javascript" src="../scripts/min/header_slider.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+		<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+		<script type="text/javascript" src="../scripts/min/header_slider.min.js"></script>
 	</body>
 </html>
